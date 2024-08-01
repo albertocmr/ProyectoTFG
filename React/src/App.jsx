75bae99ea0671967d96eb29d18faf3abc50e96ce
albertocmr <cam.ortiz.al@gmail.com>
2024-08-01 19:42:31 +0200

    App modifications to simplify code

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,34 +1,24 @@
 import React from 'react';
-import Map from './components/Map'; // Importa el componente Map
-import './styles/App.css'; // Importa los estilos globales
+import ParquesNaturales from './components/ParquesNaturales';
+import Map from './components/Map';
+import './styles/App.css';
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <div className="app-container">
       <header>
         <h1>Web Restricciones Parques Naturales-Espacios Protegidos</h1>
       </header>
+
       <main>
-        <section id="parques-naturales">
-          <h2>Parques Naturales</h2>
-          <article>
-            <h3>Sierra Nevada</h3>
-            <p>Se encuentra en Granada (Monachil)</p>
-          </article>
-          {/* Seguir añadiendo los parques naturales */}
-        </section>
-        <section id="reservas-naturales">
-          <h2>Reservas Naturales</h2>
-          <article>
-            <h3>Laguna del Chinche</h3>
-            <p>Se encuentra en Jaén (Chinche)</p>
-          </article>
-          {/* Seguir añadiendo las reservas naturales */}
-        </section>
+        <div className="parquesNaturales-container">
+          <ParquesNaturales />
+        </div>
         <div className="map-container">
           <Map />
         </div>
       </main>
+
       <footer>
         <p>&copy; 2024 Parques Naturales de Andalucía</p>
       </footer>
