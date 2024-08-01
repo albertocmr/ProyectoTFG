8128367e5f64a9db8c14feaf085b3177b5df0d3d
albertocmr <cam.ortiz.al@gmail.com>
2024-08-01 18:11:13 +0200

    Cambiamos nombre carpeta React

---
+++ React/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles/App.css'; // Importa los estilos globales
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <App />
+    </React.StrictMode>
+);
