c8ded47cec69349272be715d6369a7a2ae0b106d
albertocmr <cam.ortiz.al@gmail.com>
2024-08-02 21:40:46 +0200

    Add Tailwind CSS - Framework CSS

---
+++ React/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Escanea todos los archivos en la carpeta src para clases de Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
