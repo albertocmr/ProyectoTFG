0f5547c09a14c10ab9b73b4ad45b1429d96f8762
albertocmr <cam.ortiz.al@gmail.com>
2024-12-08 17:13:01 +0100

    Change geojson to json

--- TFG_frontend/src/assets/coordinates/geojsonImports.js
+++ TFG_frontend/src/assets/coordinates/geojsonImports.js
@@ -1,30 +1,34 @@
-import caboDeGataNijar from './cabo_de_gata_nijar.js';
-import delEstrecho from './del_estrecho.js';
-import despeniaperros from './despeniaperros.js';
-import losAlcornocales from './los_alcornocales.js';
-import naturalDoniana from './natural_doniana.js';
-import sierraAndujar from './sierra_andujar.js';
-import sierraAracena from './sierra_aracena.js';
-import sierraBaza from './sierra_baza.js';
-import cardeniaMontoro from './sierra_cardenia_montoro.js';
-import sierraCastril from './sierra_castril.js';
-import sierraCazorla from './sierra_cazorla.js';
-import sierraGrazalema from './sierra_grazalema.js';
-import sierraHornachuelos from './sierra_hornachuelos.js';
-import sierraHuetor from './sierra_huetor.js';
-import sierraMagina from './sierra_magina.js';
-import mariaLosVelez from './sierra_maria_los_velez.js';
-import sierraNevada from './sierra_nevada.js';
-import sierraNorteSevilla from './sierra_norte_sevilla.js';
-import sierraTejeda from './sierra_tejeda.js'
-import sierrasSubbeticas from './sierras_subbeticas.js';
-import sierraNieves from './sierra_nieves.js'
+import bahia_cadiz from './bahia_cadiz.json'
+import caboDeGataNijar from './cabo_de_gata_nijar.json';
+import delEstrecho from './del_estrecho.json';
+import despeniaperros from './despeniaperros.json';
+import losAlcornocales from './los_alcornocales.json';
+import montes_malaga from './montes_malaga.json';
+import naturalDoniana from './natural_doniana.json';
+import sierraAndujar from './sierra_andujar.json';
+import sierraAracena from './sierra_aracena.json';
+import sierraBaza from './sierra_baza.json';
+import cardeniaMontoro from './sierra_cardenia_montoro.json';
+import sierraCastril from './sierra_castril.json';
+import sierraCazorla from './sierra_cazorla.json';
+import sierraGrazalema from './sierra_grazalema.json';
+import sierraHornachuelos from './sierra_hornachuelos.json';
+import sierraHuetor from './sierra_huetor.json';
+import sierraMagina from './sierra_magina.json';
+import mariaLosVelez from './sierra_maria_los_velez.json';
+import sierraNevada from './sierra_nevada.json';
+import sierraNorteSevilla from './sierra_norte_sevilla.json';
+import sierraTejeda from './sierra_tejeda.json'
+import sierrasSubbeticas from './sierras_subbeticas.json';
+import sierraNieves from './sierra_nieves.json'
 
 const geojsonData = {
+  bahia_cadiz,
   caboDeGataNijar,
   delEstrecho,
   despeniaperros,
   losAlcornocales,
+  montes_malaga,
   naturalDoniana,
   sierraAndujar,
   sierraAracena,
