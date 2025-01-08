0e031df712fd46d49ebaade68d817bc5ab2d9c68
albertocmr <cam.ortiz.al@gmail.com>
2025-01-08 20:18:10 +0100

    Add geojsonImportsA.js to import and export GeoJSON data for Sierra Huetor, Baza, and Castril

---
+++ TFG_frontend/src/assets/coordinates/reserva_A/geojsonImportsA.js
@@ -0,0 +1,11 @@
+import sierraHuetorA from './sierra_huetor_A.json';
+import sierraBazaA from './sierra_baza_A.json';
+import sierraCastrilA from './sierra_castril_A.json';
+
+const geojsonDataA = {
+    sierraHuetorA,
+    sierraBazaA,
+    sierraCastrilA
+    };
+
+export default geojsonDataA;
