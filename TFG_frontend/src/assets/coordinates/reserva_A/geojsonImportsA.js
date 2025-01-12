51fab6e3d1cd1b50702f145065c266f6c0079a59
albertocmr <cam.ortiz.al@gmail.com>
2025-01-12 11:33:54 +0100

    Add new geojson data for natural park Despeñaperros and related reserves

--- TFG_frontend/src/assets/coordinates/reserva_A/geojsonImportsA.js
+++ TFG_frontend/src/assets/coordinates/reserva_A/geojsonImportsA.js
@@ -1,11 +1,19 @@
 import sierraHuetorA from './sierra_huetor_A.json';
 import sierraBazaA from './sierra_baza_A.json';
 import sierraCastrilA from './sierra_castril_A.json';
+import sierraNevadaA from './sierra_nevada_A.json';
+import sierraGrazalemaA from './sierra_grazalema_A.json';
+import despeniaperrosA from './despeniaperros_A.json';
+import sierraCardeniaMontoroA from './sierra_cardenia_montoro_A.json';
 
 const geojsonDataA = {
     sierraHuetorA,
     sierraBazaA,
-    sierraCastrilA
+    sierraCastrilA,
+    sierraNevadaA,
+    sierraGrazalemaA,
+    despeniaperrosA,
+    sierraCardeniaMontoroA
 };
 
 export default geojsonDataA;
