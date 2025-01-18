45f3c9f7e3c27c5a1c9747d1b2b8f206112fb4db
albertocmr <cam.ortiz.al@gmail.com>
2025-01-18 11:20:43 +0100

    Refactor CheckRoute script to dynamically load GPX files, create segments for points within park boundaries, and clean up uploaded GPX files after processing.

--- TFG_backend/src/scripts/CheckRoute.py
+++ TFG_backend/src/scripts/CheckRoute.py
@@ -3,11 +3,12 @@ import gpxpy
 import gpxpy.gpx
 import pandas as pd
 import os
-from datetime import datetime
 
-# Path to the GPX file for the route
-# TORRE: gpx_file = "D:/Escritorio/TrabajoFinDeGrado/Proyecto_TFG/TFG_frontend/src/assets/images/route.gpx" 
-gpx_file = "C:/Users/HP/Desktop/TrabajoFinDeGrado/Proyecto_TFG/TFG_backend/uploads/route.gpx"
+for file in os.listdir("uploads"):
+    if file.endswith(".gpx"):
+        gpx_file = os.path.join("uploads", file)
+
+# gpx_file = "C:/Users/HP/Desktop/TrabajoFinDeGrado/Proyecto_TFG/TFG_backend/uploads/route.gpx"
 gpx = gpxpy.parse(open(gpx_file, 'r'))
 
 # Extract the points from the GPX route file
@@ -17,79 +18,72 @@ for track in gpx.tracks:
         for point in segment.points:
             points.append((point.latitude, point.longitude))
 
-# Convert the points to a GeoDataFrame
-df_points = pd.DataFrame(points, columns=['latitude', 'longitude'])  # Use pandas to create a dataframe
-gdf_points = gpd.GeoDataFrame(df_points, geometry=gpd.points_from_xy(df_points.longitude, df_points.latitude))  # Convert to GeoDataFrame with GeoPandas
+df_points = pd.DataFrame(points, columns=['latitude', 'longitude'])
+gdf_points = gpd.GeoDataFrame(df_points, geometry=gpd.points_from_xy(df_points.longitude, df_points.latitude))
 gdf_points.crs = 'EPSG:4326'  # Ensure that the CRS is correctly defined
 
-# Path to the directory containing the GeoJSON files
+# geojson_dir = "C:/Users/HP/Desktop/TrabajoFinDeGrado/Proyecto_TFG/TFG_frontend/src/assets/coordinates"
+# routes_dir = "C:/Users/HP/Desktop/TrabajoFinDeGrado/Proyecto_TFG/TFG_frontend/src/assets/routes"
 
-# TORRE: geojson_dir = "D:/Escritorio/TrabajoFinDeGrado/Proyecto_TFG/TFG_frontend/src/assets/coordinates/"
-# TORRE: routes_dir = "D:/Escritorio/TrabajoFinDeGrado/Proyecto_TFG/TFG_frontend/src/assets/routes/"
+geojson_dir = "../TFG_frontend/src/assets/coordinates/"
+routes_dir = "../TFG_frontend/src/assets/routes/"
 
-geojson_dir = "C:/Users/HP/Desktop/TrabajoFinDeGrado/Proyecto_TFG/TFG_frontend/src/assets/coordinates"
-routes_dir = "C:/Users/HP/Desktop/TrabajoFinDeGrado/Proyecto_TFG/TFG_frontend/src/assets/routes"
 
-# Remove content to routes_dir
 for file in os.listdir(routes_dir):
     os.remove(os.path.join(routes_dir, file))
 
-# Check if the folder exists
 if not os.path.exists(geojson_dir):
     raise FileNotFoundError(f"Error: The folder was not found at the path: {geojson_dir}")
 
-# Get the list of all .json files in the folder
 geojson_files = [f for f in os.listdir(geojson_dir) if f.endswith('.json')]
 
-# Iterate over all the GeoJSON files
 for geojson_file in geojson_files:
     # Load the GeoJSON file
     file_path = os.path.join(geojson_dir, geojson_file)
-    gdf_park = gpd.read_file(file_path)  # Load the JSON file
+    gdf_park = gpd.read_file(file_path)
     park_polygon = gdf_park.geometry.union_all()  # Merge all the polygons in case of multiple
 
-    # Ensure the CRS is the same
     gdf_park.crs = 'EPSG:4326'
 
-    # Check which points are inside the polygon
     gdf_points['within_perimeter'] = gdf_points.geometry.apply(lambda x: park_polygon.contains(x))
-    
-    # Get the points that are inside the perimeter
-    points_within = gdf_points[gdf_points['within_perimeter']]
 
-    # Extract the name of the park (the file name without the extension)
     park_name = os.path.splitext(geojson_file)[0]
 
-    # Print the points that are inside the perimeter
-    if not points_within.empty:
-        print(f"La ruta pasa por los siguientes puntos dentro del parque: {park_name}:")
-        
-        # Create a new GPX file for the points within the park
-        gpx_output = gpxpy.gpx.GPX()
-        
-        # Add metadata to the GPX
-        gpx_output.name = f"Ruta dentro del parque {park_name}"
-        gpx_output.creator = "osrm"
-        gpx_output.time = datetime.utcnow()
+    gpx_output = gpxpy.gpx.GPX()
+    gpx_output.name = f"Ruta dentro del parque natural {park_name}"
 
-        # Create a track and track segment
-        track = gpxpy.gpx.GPXTrack(name=f"Ruta {park_name}")
-        gpx_output.tracks.append(track)
-        segment = gpxpy.gpx.GPXTrackSegment()
-        track.segments.append(segment)
+    track = gpxpy.gpx.GPXTrack(name=f"Ruta {park_name}")
+    gpx_output.tracks.append(track)
 
-        for idx, row in points_within.iterrows():
-            #print(f"Latitud: {row['latitude']}, Longitud: {row['longitude']}")
-            # Add point to track segment
+    segment = None
+    inside_park = False
+
+    for idx, row in gdf_points.iterrows():
+        current_inside = row['within_perimeter']
+
+        if current_inside:
+            if not inside_park:
+                segment = gpxpy.gpx.GPXTrackSegment()
+                track.segments.append(segment)
+                inside_park = True
+
             track_point = gpxpy.gpx.GPXTrackPoint(latitude=row['latitude'], longitude=row['longitude'])
             segment.points.append(track_point)
 
-        # Write the GPX file
+        elif inside_park:
+            inside_park = False
+
+    if len(track.segments) > 0:
         output_file = f"{park_name}_points_within.gpx"
         output_path = os.path.join(routes_dir, output_file)
         with open(output_path, 'w') as f:
             f.write(gpx_output.to_xml())
         
-        print(f"Archivo GPX creado: {output_path}")
+        print(f"Archivo GPX creado con segmentos: {output_path}")
     else:
-       print(f"Ningun punto de la ruta esta dentro del parque: {park_name}.")
+        print(f"Ningun punto de la ruta esta dentro del parque: {park_name}.")
+
+# Provisional: Remove the uploaded GPX file
+for file in os.listdir("uploads"):
+    if file.endswith(".gpx"):
+        os.remove(os.path.join("uploads", file))
