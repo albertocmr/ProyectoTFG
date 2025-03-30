f7d6c42e4b8af5e23c87e631eb6e49e02ffd8d6b
albertocmr <cam.ortiz.al@gmail.com>
2025-03-30 16:49:00 +0200

    Minimal changes

--- TFG_frontend/src/components/AddNaturalParkComponent.jsx
+++ TFG_frontend/src/components/AddNaturalParkComponent.jsx
@@ -40,22 +40,28 @@ const AddNaturalParkComponent = ({ id }) => {
   };
 
   return (
-    <div className="container border rounded pt-2 pb-4 shadow">
+    <div className="container border rounded pt-2 pb-4 shadow p-3 mt-5 rounded">
       <h1 className="fs-3">{id ? "Editar Parque Natural" : "Registro de Parque Natural"}</h1>
       <form onSubmit={saveOrUpdateNaturalPark}>
-        <div className="form-group mb-2">
-          <label htmlFor="name" className="form-label">Nombre</label>
-          <input type="text" id="name" className="form-control" value={name} onChange={(e) => setName(e.target.value)} />
+        <div className="form-group mb-2 mt-2">
+          <div className="form-floating mb-3">
+            <input type="text" id="name" placeholder="" className="form-control" value={name} onChange={(e) => setName(e.target.value)} />
+            <label htmlFor="floatingInput">Nombre del parque natural</label>
+          </div>
         </div>
 
         <div className="form-group mb-2">
-          <label htmlFor="province" className="form-label">Provincia</label>
-          <input type="text" id="province" className="form-control" value={province} onChange={(e) => setProvince(e.target.value)} />
+          <div className="form-floating mb-3">
+            <input type="text" id="province" placeholder="" className="form-control" value={province} onChange={(e) => setProvince(e.target.value)} />
+            <label htmlFor="floatingInput">Provincia</label>
+          </div>
         </div>
 
         <div className="form-group mb-2">
-          <label htmlFor="perimeterfile" className="form-label">Archivo Perímetro</label>
-          <input type="text" id="perimeterfile" className="form-control" value={perimeterfile} onChange={(e) => setPerimeterfile(e.target.value)} />
+          <div className="form-floating mb-3">
+            <input type="text" id="perimeterfile" placeholder="" className="form-control" value={perimeterfile} onChange={(e) => setPerimeterfile(e.target.value)} />
+            <label htmlFor="perimeterfile">Archivo Perímetro</label>
+          </div>
         </div>
 
         <button type="submit" className="btn btn-success">{id ? "Actualizar" : "Guardar"}</button>
