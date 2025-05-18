1896236114edf061d693e099c304ff4023f9844c
albertocmr <cam.ortiz.al@gmail.com>
2025-05-18 12:34:22 +0200

    Add coordinates to backend

---
+++ TFG_backend/park_data/coordinates/reserva_A/geojsonImportsA.js
@@ -0,0 +1,21 @@
+import sierraHuetorA from './sierra_huetor_A.json';
+import sierraBazaA from './sierra_baza_A.json';
+import sierraCastrilA from './sierra_castril_A.json';
+import sierraNevadaA from './sierra_nevada_A.json';
+import sierraGrazalemaA from './sierra_grazalema_A.json';
+import despeniaperrosA from './despeniaperros_A.json';
+import sierraCardeniaMontoroA from './sierra_cardenia_montoro_A.json';
+import naturalDonianaA from './natural_doniana_A.json';
+
+const geojsonDataA = {
+    sierraHuetorA,
+    sierraBazaA,
+    sierraCastrilA,
+    sierraNevadaA,
+    sierraGrazalemaA,
+    despeniaperrosA,
+    sierraCardeniaMontoroA,
+    naturalDonianaA
+};
+
+export default geojsonDataA;
