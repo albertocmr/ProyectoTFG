161c5b87e336f9fe9f93207696b2c8d8b94a0ed9
albertocmr <cam.ortiz.al@gmail.com>
2025-06-07 20:57:59 +0200

    Skeleton loader to management db

--- TFG_frontend/src/components/ListNaturalParks.jsx
+++ TFG_frontend/src/components/ListNaturalParks.jsx
@@ -5,31 +5,28 @@ import Search from './Search';
 const ListNaturalParks = () => {
     const [naturalParks, setNaturalParks] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         listNaturalParks();
     }, []);
 
-
     const listNaturalParks = () => {
         NaturalParkService.getAllNaturalParks()
             .then(response => {
                 setNaturalParks(response.data);
-                console.log(response.data);
+                setLoading(false);
             })
             .catch(error => {
                 console.log(error);
+                setLoading(false);
             });
     };
 
     const deleteNaturalPark = (naturalParkId) => {
         NaturalParkService.deleteNaturalPark(naturalParkId)
-            .then((response) => {
-                listNaturalParks();
-            })
-            .catch(error => {
-                console.log(error);
-            });
+            .then(() => listNaturalParks())
+            .catch(error => console.log(error));
     };
 
     const filteredNaturalParks = naturalParks.filter(naturalPark =>
@@ -44,6 +41,7 @@ const ListNaturalParks = () => {
                     <a href="/addPark" className="btn btn-primary btn-sm">Agregar parque natural</a>
                     <Search onSearch={setSearchTerm} />
                 </div>
+                
                 <table className="table table-hover table-bordered table-striped">
                     <thead>
                         <tr>
@@ -54,22 +52,37 @@ const ListNaturalParks = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {filteredNaturalParks.map(park => (
-                            <tr key={park.id}>
-                                <td>{park.id}</td>
-                                <td>{park.name}</td>
-                                <td>{park.province}</td>
-                                <td>
-                                    <a href={`/editPark/${park.id}`} className="btn btn-info btn-sm">Modificar</a>
-                                    <button
-                                        style={{ marginLeft: "10px" }}
-                                        className='btn btn-danger btn-sm'
-                                        onClick={() => deleteNaturalPark(park.id)}>
-                                        Eliminar
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
+                        {loading ? (
+                            [...Array(10)].map((_, index) => (
+                                <tr key={index}>
+                                    <td><span className="placeholder col-6 placeholder-glow"></span></td>
+                                    <td><span className="placeholder col-8 placeholder-glow"></span></td>
+                                    <td><span className="placeholder col-5 placeholder-glow"></span></td>
+                                    <td>
+                                        <span className="placeholder btn btn-secondary btn-sm disabled col-4"></span>
+                                        &nbsp;
+                                        <span className="placeholder btn btn-secondary btn-sm disabled col-4"></span>
+                                    </td>
+                                </tr>
+                            ))
+                        ) : (
+                            filteredNaturalParks.map(park => (
+                                <tr key={park.id}>
+                                    <td>{park.id}</td>
+                                    <td>{park.name}</td>
+                                    <td>{park.province}</td>
+                                    <td>
+                                        <a href={`/editPark/${park.id}`} className="btn btn-info btn-sm">Modificar</a>
+                                        <button
+                                            style={{ marginLeft: "10px" }}
+                                            className='btn btn-danger btn-sm'
+                                            onClick={() => deleteNaturalPark(park.id)}>
+                                            Eliminar
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))
+                        )}
                     </tbody>
                 </table>
             </div>
